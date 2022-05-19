a1708975fbece8d6259d74703e81b3b2341739a3
“rajkumar-robosoft” <rajkumar.singh@robosoftin.com>
2022-05-19 20:00:29 +0530

    Program Structure

--- program-structure/chess-board.js
+++ program-structure/chess-board.js
@@ -1,3 +1,8 @@
+// Chessboard
+// Write a program that creates a string that represents an 8×8 grid, using newline characters to separate lines.
+// At each position of the grid there is either a space or a "#" character. The characters should form a chessboard.
+
+
 let size = 8;
 for (let i = 0; i < size; i++) {
   let str = "";
--- program-structure/triangle-looping.js
+++ program-structure/triangle-looping.js
@@ -1,3 +1,6 @@
+//Looping a triangle
+//Write a loop that makes seven calls to console.log to output the following triangle:
+
 let result = "";
 let loop = 7;
 for (let i = 0; i < loop; i++) {
